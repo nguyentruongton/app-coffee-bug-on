1b2a821772b58ec3657c1121e1d82c1e4b90a1de
stark <tonhuyen2000@gmail.com>
2022-12-05 21:28:04 +0700

    First commit

---
+++ App.js
@@ -0,0 +1,23 @@
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View } from "react-native";
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <Text>
+        Chào Tùng, anh tạo project và mời mày vào rồi. Xin lỗi vì sự chậm trễ
+        này!
+      </Text>
+      <StatusBar style="auto" />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+});
