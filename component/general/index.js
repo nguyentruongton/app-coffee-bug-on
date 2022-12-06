987401ec7be30116b641bfe3e9b2954b57513221
Tung <lehuynhphuongtung0601@gmail.com>
2022-12-06 22:42:31 +0700

    Bottom Tabs Navigation

---
+++ component/general/index.js
@@ -0,0 +1,12 @@
+import React from "react";
+import { Text, View } from "react-native";
+import styles from "./index.css";
+const General = () => {
+    return(
+        <View style={styles.container}>
+            <Text>Cài đặt chung</Text>
+        </View>
+    );
+}
+
+export default General;
