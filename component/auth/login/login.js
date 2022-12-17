a735b7f0e7b2967cefe9d959c39bf408d5fa7d7f
Tung <lehuynhphuongtung0601@gmail.com>
2022-12-17 19:08:09 +0700

    update form

--- component/auth/login/login.js
+++ component/auth/login/login.js
@@ -1,10 +1,36 @@
 import React from 'react';
-import { View,Text } from 'react-native';
+import { View,Text,StyleSheet,TextInput, TouchableOpacity } from 'react-native';
 const Login = () => {
     return (
         <View>
             <Text>Trang đăng nhập</Text>
+            <TextInput
+                style={styles.input}
+                // onChangeText={onChangeNumber}
+                // value={number}
+                placeholder="useless placeholder"
+                keyboardType="numeric"
+            />
+            <TextInput
+                style={styles.input}
+                // onChangeText={onChangeNumber}
+                // value={number}
+                placeholder="useless placeholder"
+                keyboardType="numeric"
+            />
+            <TouchableOpacity>
+                <Text>Đăng nhập</Text>
+            </TouchableOpacity>
         </View>
     )
 };
 export default Login;
+
+const styles = StyleSheet.create({
+  input: {
+    height: 40,
+    margin: 12,
+    borderWidth: 1,
+    padding: 10,
+  },
+});
