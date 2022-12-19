4fb35f7a0215228c2d603fd58fa2f7e06243cb28
Tung <lehuynhphuongtung0601@gmail.com>
2022-12-19 21:02:50 +0700

    update mainpage

--- component/mainPage/mainPage.js
+++ component/mainPage/mainPage.js
@@ -1,8 +1,9 @@
 import React from "react";
-import { Text, View, ScrollView ,Image} from "react-native";
+import { Text, View, ScrollView ,Image, Dimensions, TouchableOpacity} from "react-native";
 import Panel from "./panel";
-
+const { height, width } = Dimensions.get('window')
 const MainPage = () => {
+    const { height, width } = Dimensions.get('window')
     return(
         <View>
             <ScrollView 
@@ -32,6 +33,50 @@ const MainPage = () => {
                         source={{uri:`https://file.hstatic.net/1000075078/file/cloudtea_1_5dc49fd17ba04030993d2f797dc570f2.png`}}
                     />
                 </View>
+            <View style={{paddingHorizontal:20, marginTop:20, flexDirection:'row', flexWrap:'wrap'}}>
+                <TouchableOpacity>
+                    <View style={{ width: width / 2 - 30, height: height / 2 - 100, borderWidth: 0.5, borderColor: '#dddddd' ,margin: 5}}>
+                        <View style={{ flex: 1 }}>
+                            <Image
+                                style={{ flex: 1, width: null, height: null, resizeMode: 'cover' }}
+                                source={{uri:`https://product.hstatic.net/1000075078/product/1665655345_tch-sua-da_e0737a64b29e452f9c7eadb23300821a.jpg`}} />
+                        </View>
+                        <View style={{ flex: 0.3, alignItems: 'flex-start', justifyContent: 'space-evenly', paddingLeft: 10 }}>
+                            <Text style={{ fontSize: 10, color: '#b63838' }}>Cafe viet nam</Text>
+                            <Text style={{ fontSize: 12, fontWeight: 'bold' }}>Ca phe sua da</Text>
+                            <Text style={{ fontSize: 10 }}>25.000$</Text>
+                        </View>
+                    </View>
+                </TouchableOpacity>
+                <TouchableOpacity>
+                    <View style={{ width: width / 2 - 30, height: height / 2 - 100, borderWidth: 0.5, borderColor: '#dddddd' ,margin: 5}}>
+                        <View style={{ flex: 1 }}>
+                            <Image
+                                style={{ flex: 1, width: null, height: null, resizeMode: 'cover' }}
+                                source={{uri:`https://product.hstatic.net/1000075078/product/1665655345_tch-sua-da_e0737a64b29e452f9c7eadb23300821a.jpg`}} />
+                        </View>
+                        <View style={{ flex: 0.3, alignItems: 'flex-start', justifyContent: 'space-evenly', paddingLeft: 10 }}>
+                            <Text style={{ fontSize: 10, color: '#b63838' }}>Cafe viet nam</Text>
+                            <Text style={{ fontSize: 12, fontWeight: 'bold' }}>Ca phe sua da</Text>
+                            <Text style={{ fontSize: 10 }}>25.000$</Text>
+                        </View>
+                    </View>
+                </TouchableOpacity>
+                <TouchableOpacity>
+                    <View style={{ width: width / 2 - 30, height: height / 2 - 100, borderWidth: 0.5, borderColor: '#dddddd' ,margin: 5}}>
+                        <View style={{ flex: 1 }}>
+                            <Image
+                                style={{ flex: 1, width: null, height: null, resizeMode: 'cover' }}
+                                source={{uri:`https://product.hstatic.net/1000075078/product/1665655345_tch-sua-da_e0737a64b29e452f9c7eadb23300821a.jpg`}} />
+                        </View>
+                        <View style={{ flex: 0.3, alignItems: 'flex-start', justifyContent: 'space-evenly', paddingLeft: 10 }}>
+                            <Text style={{ fontSize: 10, color: '#b63838' }}>Cafe viet nam</Text>
+                            <Text style={{ fontSize: 12, fontWeight: 'bold' }}>Ca phe sua da</Text>
+                            <Text style={{ fontSize: 10 }}>25.000$</Text>
+                        </View>
+                    </View>
+                </TouchableOpacity>
+            </View>
             </ScrollView>
         </View>
     );
