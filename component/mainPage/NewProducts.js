5265d230bdf8b667f64d78d1a27d0a4e86a7cb96
Stark <tonhuyen2000@gmail.com>
2022-12-26 00:31:02 +0700

    Improved UI Home page

---
+++ component/mainPage/NewProducts.js
@@ -0,0 +1,139 @@
+import React from "react";
+import { Text, View, Dimensions, StyleSheet } from "react-native";
+import { Button, Card } from "react-native-paper";
+
+const NewProducts = () => {
+  const { height, width } = Dimensions.get("window");
+
+  const styles = StyleSheet.create({
+    namePro: { fontSize: 22, fontWeight: "600", marginBottom: 2 },
+    nameCategory: { fontSize: 14, fontWeight: "200", marginBottom: 12 },
+    price: { fontSize: 18, fontWeight: "500" },
+  });
+
+  return (
+    <View>
+      <Text
+        style={{
+          fontSize: 28,
+          paddingTop: 16,
+          paddingLeft: 16,
+          fontWeight: "600",
+          textTransform: "uppercase",
+          color: "#3D685E",
+        }}
+      >
+        Sản phẩm mới
+      </Text>
+      <View
+        style={{
+          paddingHorizontal: 16,
+          marginTop: 16,
+          flexDirection: "row",
+          flexWrap: "wrap",
+          display: "flex",
+          justifyContent: "space-between",
+        }}
+      >
+        <Card
+          mode="outlined"
+          style={{
+            width: width / 2 - 25,
+            height: height / 2 - 126,
+            marginBottom: 16,
+          }}
+        >
+          <Card.Cover
+            source={{
+              uri: "https://product.hstatic.net/1000075078/product/1665655345_tch-sua-da_e0737a64b29e452f9c7eadb23300821a.jpg",
+            }}
+          />
+          <View style={{ padding: 16 }}>
+            <Text style={styles.namePro}>Cà phê sữa đá</Text>
+            <Text style={styles.nameCategory}>Cà phê Việt Nam</Text>
+            <Text style={styles.price}>25.000 đ</Text>
+          </View>
+          <Card.Actions>
+            <Button onPress={() => {}} icon={"plus"} mode="outlined">
+              Thêm
+            </Button>
+          </Card.Actions>
+        </Card>
+        <Card
+          mode="outlined"
+          style={{
+            width: width / 2 - 25,
+            height: height / 2 - 126,
+            marginBottom: 16,
+          }}
+        >
+          <Card.Cover
+            source={{
+              uri: "https://product.hstatic.net/1000075078/product/1665655345_tch-sua-da_e0737a64b29e452f9c7eadb23300821a.jpg",
+            }}
+          />
+          <View style={{ padding: 16 }}>
+            <Text style={styles.namePro}>Cà phê sữa đá</Text>
+            <Text style={styles.nameCategory}>Cà phê Việt Nam</Text>
+            <Text style={styles.price}>25.000 đ</Text>
+          </View>
+          <Card.Actions>
+            <Button onPress={() => {}} icon={"plus"} mode="outlined">
+              Thêm
+            </Button>
+          </Card.Actions>
+        </Card>
+        <Card
+          mode="outlined"
+          style={{
+            width: width / 2 - 25,
+            height: height / 2 - 126,
+            marginBottom: 16,
+          }}
+        >
+          <Card.Cover
+            source={{
+              uri: "https://product.hstatic.net/1000075078/product/1665655345_tch-sua-da_e0737a64b29e452f9c7eadb23300821a.jpg",
+            }}
+          />
+          <View style={{ padding: 16 }}>
+            <Text style={styles.namePro}>Cà phê sữa đá</Text>
+            <Text style={styles.nameCategory}>Cà phê Việt Nam</Text>
+            <Text style={styles.price}>25.000 đ</Text>
+          </View>
+          <Card.Actions>
+            <Button onPress={() => {}} icon={"plus"} mode="outlined">
+              Thêm
+            </Button>
+          </Card.Actions>
+        </Card>
+        <Card
+          mode="outlined"
+          style={{
+            width: width / 2 - 25,
+            height: height / 2 - 126,
+            marginBottom: 16,
+          }}
+        >
+          <Card.Cover
+            source={{
+              uri: "https://product.hstatic.net/1000075078/product/1665655345_tch-sua-da_e0737a64b29e452f9c7eadb23300821a.jpg",
+            }}
+          />
+          <View style={{ padding: 16 }}>
+            <Text style={styles.namePro}>Cà phê sữa đá</Text>
+            <Text style={styles.nameCategory}>Cà phê Việt Nam</Text>
+            <Text style={styles.price}>25.000 đ</Text>
+          </View>
+          <Card.Actions>
+            <Button onPress={() => {}} icon={"plus"} mode="outlined">
+              Thêm
+            </Button>
+          </Card.Actions>
+        </Card>
+      </View>
+    </View>
+  );
+};
+
+export default NewProducts;
--- component/mainPage/mainPage.js
+++ component/mainPage/mainPage.js
@@ -1,85 +1,89 @@
 import React from "react";
-import { Text, View, ScrollView ,Image, Dimensions, TouchableOpacity} from "react-native";
-import Panel from "./panel";
-const { height, width } = Dimensions.get('window')
+import {
+  Text,
+  View,
+  ScrollView,
+  Image,
+  Dimensions,
+  StatusBar,
+} from "react-native";
+import Panel from "./Panel";
+import { Appbar } from "react-native-paper";
+import NewProducts from "./NewProducts";
+
 const MainPage = () => {
-    const { height, width } = Dimensions.get('window')
-    return(
-        <View>
-            <ScrollView 
-                showsVerticalScrollIndicator={false}
+  const { height, width } = Dimensions.get("window");
+
+  return (
+    <View>
+      <StatusBar currentHeight />
+      <Appbar.Header>
+        <Appbar.Content title="Cà phê BUG ỔN" />
+        <Appbar.Action icon="magnify" onPress={() => {}} />
+      </Appbar.Header>
+      <ScrollView centerContent={true} showsVerticalScrollIndicator={false}>
+        <Panel />
+        <NewProducts />
+        <View
+          style={{
+            padding: 16,
+          }}
+        >
+          <Text
+            style={{
+              fontSize: 28,
+              fontWeight: "600",
+              textTransform: "uppercase",
+              color: "#3D685E",
+              marginBottom: 16,
+            }}
+          >
+            Giới thiệu
+          </Text>
+          <View>
+            <Image
+              style={{
+                width: "100%",
+                height: 110,
+                // height: height / 7,
+                resizeMode: "contain",
+                marginBottom: 4,
+              }}
+              source={{
+                uri: `https://file.hstatic.net/1000075078/file/cloudtea_tagline_1_c39d8fd0d46b4144be9cebd05e318796.png`,
+              }}
+            />
+            <Image
+              style={{
+                width: "100%",
+                height: 400,
+                // height: height / 3,
+                resizeMode: "contain",
+              }}
+              source={{
+                uri: `https://file.hstatic.net/1000075078/file/cloudtea_1_5dc49fd17ba04030993d2f797dc570f2.png`,
+              }}
+            />
+            <Text
+              style={{
+                fontSize: 14,
+                fontWeight: "400",
+                textAlign: "justify",
+                marginTop: 12,
+                lineHeight: 20,
+                marginBottom: 60,
+              }}
             >
-                <Panel/>
-                <View style={{justifyContent: 'center', alignItems: 'center'}}>
-                    <Image
-                        style={{
-                            width: 300,
-                            height: 75,
-                            marginTop: 20, 
-                            flex:1 ,
-                            resizeMode:'cover'
-                        }}
-                        source={{uri:`https://file.hstatic.net/1000075078/file/cloudtea_tagline_1_c39d8fd0d46b4144be9cebd05e318796.png`}}
-                    />
-                </View>
-                <View style={{justifyContent: 'center', alignItems: 'center'}}>
-                    <Image
-                        style={{ 
-                            flex: 1, 
-                            width: 200,
-                            height: 200,
-                            resizeMode:'cover'
-                        }}
-                        source={{uri:`https://file.hstatic.net/1000075078/file/cloudtea_1_5dc49fd17ba04030993d2f797dc570f2.png`}}
-                    />
-                </View>
-            <View style={{paddingHorizontal:20, marginTop:20, flexDirection:'row', flexWrap:'wrap'}}>
-                <TouchableOpacity>
-                    <View style={{ width: width / 2 - 30, height: height / 2 - 100, borderWidth: 0.5, borderColor: '#dddddd' ,margin: 5}}>
-                        <View style={{ flex: 1 }}>
-                            <Image
-                                style={{ flex: 1, width: null, height: null, resizeMode: 'cover' }}
-                                source={{uri:`https://product.hstatic.net/1000075078/product/1665655345_tch-sua-da_e0737a64b29e452f9c7eadb23300821a.jpg`}} />
-                        </View>
-                        <View style={{ flex: 0.3, alignItems: 'flex-start', justifyContent: 'space-evenly', paddingLeft: 10 }}>
-                            <Text style={{ fontSize: 10, color: '#b63838' }}>Cafe viet nam</Text>
-                            <Text style={{ fontSize: 12, fontWeight: 'bold' }}>Ca phe sua da</Text>
-                            <Text style={{ fontSize: 10 }}>25.000$</Text>
-                        </View>
-                    </View>
-                </TouchableOpacity>
-                <TouchableOpacity>
-                    <View style={{ width: width / 2 - 30, height: height / 2 - 100, borderWidth: 0.5, borderColor: '#dddddd' ,margin: 5}}>
-                        <View style={{ flex: 1 }}>
-                            <Image
-                                style={{ flex: 1, width: null, height: null, resizeMode: 'cover' }}
-                                source={{uri:`https://product.hstatic.net/1000075078/product/1665655345_tch-sua-da_e0737a64b29e452f9c7eadb23300821a.jpg`}} />
-                        </View>
-                        <View style={{ flex: 0.3, alignItems: 'flex-start', justifyContent: 'space-evenly', paddingLeft: 10 }}>
-                            <Text style={{ fontSize: 10, color: '#b63838' }}>Cafe viet nam</Text>
-                            <Text style={{ fontSize: 12, fontWeight: 'bold' }}>Ca phe sua da</Text>
-                            <Text style={{ fontSize: 10 }}>25.000$</Text>
-                        </View>
-                    </View>
-                </TouchableOpacity>
-                <TouchableOpacity>
-                    <View style={{ width: width / 2 - 30, height: height / 2 - 100, borderWidth: 0.5, borderColor: '#dddddd' ,margin: 5}}>
-                        <View style={{ flex: 1 }}>
-                            <Image
-                                style={{ flex: 1, width: null, height: null, resizeMode: 'cover' }}
-                                source={{uri:`https://product.hstatic.net/1000075078/product/1665655345_tch-sua-da_e0737a64b29e452f9c7eadb23300821a.jpg`}} />
-                        </View>
-                        <View style={{ flex: 0.3, alignItems: 'flex-start', justifyContent: 'space-evenly', paddingLeft: 10 }}>
-                            <Text style={{ fontSize: 10, color: '#b63838' }}>Cafe viet nam</Text>
-                            <Text style={{ fontSize: 12, fontWeight: 'bold' }}>Ca phe sua da</Text>
-                            <Text style={{ fontSize: 10 }}>25.000$</Text>
-                        </View>
-                    </View>
-                </TouchableOpacity>
-            </View>
-            </ScrollView>
+              Vừa êm mượt dịu dàng, vừa bùng nổ nồng nàn, Trà Sữa CloudTea Hương
+              Vị Nụ Hôn Đầu mang đến trải nghiệm đầy mới mẻ. Chạm môi là bồng
+              bềnh, càng thêm đậm đà nhờ topping vụn bánh quy phô mai hoặc bột
+              ca cao thơm lừng.
+            </Text>
+          </View>
         </View>
-    );
-}
+      </ScrollView>
+    </View>
+  );
+};
 
 export default MainPage;
