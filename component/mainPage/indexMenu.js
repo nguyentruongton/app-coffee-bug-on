1646059e48e261fb5e824ba4798c3fa5644b1b19
Tung <lehuynhphuongtung0601@gmail.com>
2022-12-28 02:40:27 +0700

    update mainpage

--- component/mainPage/indexMenu.js
+++ component/mainPage/indexMenu.js
@@ -2,12 +2,25 @@ import React from "react";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
 import MainPage from "../mainPage/mainPage";
 import ItemDetail from "../itemDetail/itemDetail";
-import NewProducts from "./NewProducts";
+import { Appbar } from "react-native-paper";
+import styles from "../../BugOnStyles";
+
+const CustomNavigationBar = ({ navigation, back }) => {
+  return (
+    <Appbar.Header>
+      {back ? <Appbar.BackAction onPress={navigation.goBack} /> : null}
+      <Appbar.Content titleStyle={styles.titleAppBar} title="Chi tiết sản phẩm" />
+    </Appbar.Header>
+  );
+};
 
 const IndexMenu = () => {
   const Stack = createNativeStackNavigator();
   return (
-    <Stack.Navigator>
+    <Stack.Navigator
+      initialRouteName="Chi tiết sản phẩm"
+      screenOptions={{ header: (props) => <CustomNavigationBar {...props} /> }}
+    >
       <Stack.Screen name="Trang chủ" component={MainPage} options={{headerShown:false}} />
       <Stack.Screen name="Chi tiết" component={ItemDetail} options={{headerShown:true}}/>
     </Stack.Navigator>
